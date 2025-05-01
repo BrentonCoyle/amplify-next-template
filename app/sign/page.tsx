af4ce4d1dcf531f446f41d85456bc2937384f4d6
Brenton <brenton_coyle@stu.indianhills.edu>
2025-04-30 19:33:08 -0500

    added button to go add page

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -1,16 +1,18 @@
 // app/about/page.tsx
+"use client";
 export default function SignInPage() {
     return (
         <div>
             <h1>Sign in Page</h1>
             <p>This is where I will have the C.R.U.D stuff for movies only people with access can rewiew/add movies</p>
 
+            <button onClick={() => window.location.href = "sign/add"} >Go to Add Page</button>
 
 
 
 
 
-        
+
 
 
 
