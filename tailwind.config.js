ed95e62d920864c5e826db27ef9feae003c90fa6
Brenton <brenton_coyle@stu.indianhills.edu>
2025-05-13 21:10:07 -0500

    fixed file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     content: [
       "./app/**/*.{js,ts,jsx,tsx}", 
-      "./components/**/*.{js,ts,jsx,tsx}",
+      "./Components/**/*.{js,ts,jsx,tsx}",
       "./admin/**/*.{js,ts,jsx,tsx}",
       "./movies/**/*.{js,ts,jsx,tsx}",
       "./sign/**/*.{js,ts,jsx,tsx}",
